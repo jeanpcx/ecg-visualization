e63cc536434f469029f90b39309f6ec8fc97fdef
Jean C. <polojean02@gmail.com>
2024-07-16 23:49:55 -0500

    map_location=torch.device('cpu')

--- model.py
+++ model.py
@@ -86,7 +86,7 @@ def get_activation(name):
 
 def load_model():
   model = CNN(num_classes = 5, hid_size = 128)
-  model.load_state_dict(torch.load('data/model_50_dict.pth'))
+  model.load_state_dict(torch.load('data/model_50_dict.pth', map_location=torch.device('cpu')))
   # model.load_state_dict()
   
   # model = torch.load('data/model_50.pth', map_location='cpu', pickle_module={'CNN': CNN})
